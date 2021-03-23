21bb1e18e0c4a0a5b7fecc5e83958570ef6df6aa
mlazarte <mlazarte@practia.global>
2021-03-23 18:56:29 -0300

    Added Router to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage> 
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }

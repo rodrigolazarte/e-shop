a35d15504d19b1f962725c37bb96f5d5150a48c4
mlazarte <martinlazarte@alu.frt.utn.edu.ar>
2021-03-24 22:32:56 -0300

    updated menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
-        <Route path='/hats' component={HatsPage}></Route>
+        <Route path='/shop/hats' component={HatsPage}></Route>
       </Switch>
     </div>
   );

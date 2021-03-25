7712f6b52a2dc90593700df79ca5b5b5176d49d6
mlazarte <martinlazarte@alu.frt.utn.edu.ar>
2021-03-24 23:17:28 -0300

    Added collection preview component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
-        <Route path='/shop/hats' component={HatsPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );

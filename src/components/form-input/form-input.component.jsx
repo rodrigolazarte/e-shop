096413b0f3f8877bbcdc02fbc0d3b6a0da88040f
mlazarte <martinlazarte@alu.frt.utn.edu.ar>
2021-04-04 19:47:14 -0300

    Added redux libraries. Modified styles of sign in sign up page

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,16 +5,16 @@ import './form-input.styles.scss';
 const FormInput = ({handleChange, label, ...otherProps}) => (
     <div className='group'>
         <input className='form-input' onChange={handleChange} {...otherProps}></input>
-        {
-            label ? 
-            (<label className={`${otherProps.value.lenght ? 'shrink' : ''} form-input-label`}>
-                {
-                    label
-                }
-            </label>)
-            : null
-        }
+        { label ? (
+            <label 
+                className={`${
+                    otherProps.value.length ? 'shrink' : ''
+                } form-input-label`}
+            >
+                {label}
+            </label>
+        ) : null}
     </div>
-)
+);
 
 export default FormInput;
